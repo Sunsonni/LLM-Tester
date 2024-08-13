1c307fd046c23097250f4d50d65b63d9755733d4
Elizabeth Levinson <lizarose266@gmail.com>
2024-08-13 12:47:15 -0500

    First commit, added npm install, added images folder in frontend, added Episode logo

---
+++ LLM Tester -client/LLM Tester/src/Components/header/index.jsx
@@ -0,0 +1,14 @@
+import "./headerStyles.css"
+
+const Header = () => {
+    return (
+       <>
+          <header>
+                <img id="logo" src="/images/episode_logo.png" alt="Episode Logo" />
+          </header>
+       </>
+    )
+}
+
+export default Header;
+ 
--- LLM Tester -client/LLM Tester/src/Components/navBar/navBar.jsx
+++ LLM Tester -client/LLM Tester/src/Components/navBar/navBar.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import '../../App.css';
+import Header from '../header';
+import "./navStyles.css"
 
 const SideNav = (props)=> {
 return(
+    <div className='topNav'>
+        <Header></Header>
+    
     <div className="sidenav">
+        
         <a href="#section">About</a>
         <a href="#section">Services</a>
         <a href="#section">Contact</a>
+    </div>
     </div>
 );
 };
