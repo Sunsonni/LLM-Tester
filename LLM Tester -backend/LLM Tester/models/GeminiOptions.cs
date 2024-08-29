7bd1b8ca88933c30d0fe34cc43ed6d4a947cb36a
sunsonni <sonniemnguyen@gmail.com>
2024-08-29 00:34:12 -0500

    idk

--- LLM Tester -backend/LLM Tester/models/GeminiOptions.cs
+++
@@ -1,7 +0,0 @@
-namespace LLM_Tester.models;
-
-internal sealed class GeminiOptions
-{
-    public string ApiKey { get; set; } = string.Empty;
-    public string Url { get; set; } = string.Empty;
-}
